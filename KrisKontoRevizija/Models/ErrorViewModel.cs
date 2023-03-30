fc983c7582c756262b97e3903dee16f16fdcf83b
Smladenovska <100212774+Smladenovska@users.noreply.github.com>
2023-03-30 22:08:56 +0200

    commit

---
+++ KrisKontoRevizija/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KrisKontoRevizija.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
